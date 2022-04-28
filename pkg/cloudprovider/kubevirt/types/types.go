08ed810e7e74766f55341409363533d4e751b48c
mlavacca <lavacca.mattia@gmail.com>
2022-04-28 10:24:47 +0200

    New Kubevirt CloudProviderSpec
    Update of the Kubevirt cloudProviderSpec with the new fields introduced by the
    latest release of machine-controller: 1.48.0.
    
    Signed-off-by: Mattia Lavacca <lavacca.mattia@gmail.com>

--- pkg/cloudprovider/kubevirt/types/types.go
+++ pkg/cloudprovider/kubevirt/types/types.go
@@ -16,9 +16,72 @@ limitations under the License.
 
 package types
 
-import "github.com/kubermatic/machine-controller/pkg/providerconfig/types"
+import (
+	providerconfigtypes "github.com/kubermatic/machine-controller/pkg/providerconfig/types"
 
-// RawConfig is a direct representation of an Kubevirt machine object's configuration
+	corev1 "k8s.io/api/core/v1"
+)
+
 type RawConfig struct {
-	Kubeconfig types.ConfigVarString `json:"kubeconfig,omitempty"`
+	Auth           Auth           `json:"auth,omitempty"`
+	VirtualMachine VirtualMachine `json:"virtualMachine,omitempty"`
+	Affinity       Affinity       `json:"affinity,omitempty"`
+}
+
+// Auth
+type Auth struct {
+	Kubeconfig providerconfigtypes.ConfigVarString `json:"kubeconfig,omitempty"`
+}
+
+// VirtualMachine
+type VirtualMachine struct {
+	Flavor    Flavor                              `json:"flavor,omitempty"`
+	Template  Template                            `json:"template,omitempty"`
+	DNSPolicy providerconfigtypes.ConfigVarString `json:"dnsPolicy,omitempty"`
+	DNSConfig *corev1.PodDNSConfig                `json:"dnsConfig,omitempty"`
+}
+
+// Flavor
+type Flavor struct {
+	Name    providerconfigtypes.ConfigVarString `json:"name,omitempty"`
+	Profile providerconfigtypes.ConfigVarString `json:"profile,omitempty"`
+}
+
+// Template
+type Template struct {
+	CPUs           providerconfigtypes.ConfigVarString `json:"cpus,omitempty"`
+	Memory         providerconfigtypes.ConfigVarString `json:"memory,omitempty"`
+	PrimaryDisk    PrimaryDisk                         `json:"primaryDisk,omitempty"`
+	SecondaryDisks []SecondaryDisks                    `json:"secondaryDisks,omitempty"`
+}
+
+// PrimaryDisk
+type PrimaryDisk struct {
+	Disk
+	OsImage providerconfigtypes.ConfigVarString `json:"osImage,omitempty"`
+}
+
+// SecondaryDisks
+type SecondaryDisks struct {
+	Disk
+}
+
+// Disk
+type Disk struct {
+	Size             providerconfigtypes.ConfigVarString `json:"size,omitempty"`
+	StorageClassName providerconfigtypes.ConfigVarString `json:"storageClassName,omitempty"`
+}
+
+// Affinity
+type Affinity struct {
+	PodAffinityPreset     providerconfigtypes.ConfigVarString `json:"podAffinityPreset,omitempty"`
+	PodAntiAffinityPreset providerconfigtypes.ConfigVarString `json:"podAntiAffinityPreset,omitempty"`
+	NodeAffinityPreset    NodeAffinityPreset                  `json:"nodeAffinityPreset,omitempty"`
+}
+
+// NodeAffinityPreset
+type NodeAffinityPreset struct {
+	Type   providerconfigtypes.ConfigVarString   `json:"type,omitempty"`
+	Key    providerconfigtypes.ConfigVarString   `json:"key,omitempty"`
+	Values []providerconfigtypes.ConfigVarString `json:"values,omitempty"`
 }
