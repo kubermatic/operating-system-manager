13bdd3ad9737050a49023741a3b70258135296c2
Waleed Malik <ahmedwaleedmalik@gmail.com>
2022-06-08 09:08:30 +0500

    Synchronize KubeVirt with machine-controller
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/cloudprovider/kubevirt/provider.go
+++ pkg/cloudprovider/kubevirt/provider.go
@@ -17,14 +17,17 @@ limitations under the License.
 package kubevirt
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
 
 	providerconfigtypes "github.com/kubermatic/machine-controller/pkg/providerconfig/types"
 
 	"k8c.io/operating-system-manager/pkg/cloudprovider/kubevirt/types"
 	"k8c.io/operating-system-manager/pkg/providerconfig/config"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -59,14 +62,48 @@ func getConfig(pconfig providerconfigtypes.Config) (*types.CloudConfig, error) {
 		err  error
 	)
 
-	opts.Kubeconfig, err = config.GetConfigVarResolver().GetConfigVarStringValueOrEnv(rawConfig.Auth.Kubeconfig, envKubevirtKubeconfig)
-	if err != nil {
-		return nil, fmt.Errorf(`failed to get value of "kubeconfig" field: %w`, err)
+	// Kubeconfig was specified directly in the Machine/MachineDeployment CR. In this case we need to ensure that the value is base64 encoded.
+	if rawConfig.Auth.Kubeconfig.Value != "" {
+		val, err := base64.StdEncoding.DecodeString(rawConfig.Auth.Kubeconfig.Value)
+		if err != nil {
+			// An error here means that this is not a valid base64 string
+			// We can be more explicit here with the error for visibility. Webhook will return this error if we hit this scenario.
+			return nil, fmt.Errorf("failed to decode base64 encoded kubeconfig. Expected value is a base64 encoded Kubeconfig in JSON or YAML format: %w", err)
+		}
+		opts.Kubeconfig = string(val)
+	} else {
+		// Environment variable or secret reference was used for providing the value of kubeconfig
+		// We have to be lenient in this case and allow unencoded values as well.
+		opts.Kubeconfig, err = config.GetConfigVarResolver().GetConfigVarStringValueOrEnv(rawConfig.Auth.Kubeconfig, envKubevirtKubeconfig)
+		if err != nil {
+			return nil, fmt.Errorf(`failed to get value of "kubeconfig" field: %w`, err)
+		}
+		val, err := base64.StdEncoding.DecodeString(opts.Kubeconfig)
+		// We intentionally ignore errors here with an assumption that an unencoded YAML or JSON must have been passed on
+		// in this case.
+		if err == nil {
+			opts.Kubeconfig = string(val)
+		}
 	}
+
+	opts.Namespace = getNamespace()
 
 	cloudConfig := &types.CloudConfig{
 		Global: opts,
 	}
 
 	return cloudConfig, nil
+}
+
+// getNamespace returns the namespace where the VM is created.
+// VM is created in a dedicated namespace <cluster-id>
+// which is the namespace where the machine-controller pod is running.
+// Defaults to `kube-system`.
+func getNamespace() string {
+	ns := os.Getenv("POD_NAMESPACE")
+	if ns == "" {
+		// Useful especially for ci tests.
+		ns = metav1.NamespaceSystem
+	}
+	return ns
 }
