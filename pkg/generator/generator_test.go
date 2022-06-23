b15aa9416e8db7d8e56e8398f57f074fae207b75
Moath Qasim <moad.qassem@gmail.com>
2022-06-23 19:36:05 +0200

    Support yum repos cloud-init module (#177)
    * support yum repos cloud-init module
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>
    
    * adding tests for yum repos module
    
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- pkg/generator/generator_test.go
+++ pkg/generator/generator_test.go
@@ -103,7 +103,8 @@ runcmd:
 
 rh_subscription:
     password: test_password
-    username: test_username`),
+    username: test_username
+`),
 		},
 		{
 			name: "generated cloud-init for ubuntu without a service",
@@ -224,6 +225,91 @@ runcmd:
 				},
 			},
 			expectedCloudConfig: []byte(`{"ignition":{"config":{},"security":{"tls":{}},"timeouts":{},"version":"2.3.0"},"networkd":{},"passwd":{"users":[{"name":"core","sshAuthorizedKeys":["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDR3","ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDR4"]}]},"storage":{"files":[{"filesystem":"root","path":"/opt/bin/test.service","contents":{"source":"data:,%23!%2Fbin%2Fbash%0Aset%20-xeuo%20pipefail%0Acloud-init%20clean%0Acloud-init%20init%0Asystemctl%20start%20provision.service%0A","verification":{}},"mode":448},{"filesystem":"root","path":"/opt/bin/setup.service","contents":{"source":"data:,%23!%2Fbin%2Fbash%0Aset%20-xeuo%20pipefail%0Acloud-init%20clean%0Acloud-init%20init%0Asystemctl%20start%20provision.service%0A","verification":{}},"mode":448}]},"systemd":{}}`),
+		},
+		{
+			name: "generated cloud-init modules for rhel",
+			osc: &osmv1alpha1.OperatingSystemConfig{
+				Spec: osmv1alpha1.OperatingSystemConfigSpec{
+					OSName:    "rhel",
+					OSVersion: "8.5",
+					Files: []osmv1alpha1.File{
+						{
+							Path:        "/opt/bin/test.service",
+							Permissions: pointer.Int32Ptr(0700),
+							Content: osmv1alpha1.FileContent{
+								Inline: &osmv1alpha1.FileContentInline{
+									Data: "    #!/bin/bash\n    set -xeuo pipefail\n    cloud-init clean\n    cloud-init init\n    systemctl start provision.service",
+								},
+							},
+						},
+						{
+							Path:        "/opt/bin/setup.service",
+							Permissions: pointer.Int32Ptr(0700),
+							Content: osmv1alpha1.FileContent{
+								Inline: &osmv1alpha1.FileContentInline{
+									Data: "    #!/bin/bash\n    set -xeuo pipefail\n    cloud-init clean\n    cloud-init init\n    systemctl start provision.service",
+								},
+							},
+						},
+					},
+					UserSSHKeys: []string{
+						"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDR3",
+						"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDR4",
+					},
+					CloudInitModules: &osmv1alpha1.CloudInitModule{
+						BootCMD:        []string{"echo hello-world", "echo hello-osm"},
+						RHSubscription: map[string]string{"username": "test_username", "password": "test_password"},
+						RunCMD:         []string{"systemctl restart test.service", "systemctl restart setup.service", "systemctl daemon-reload"},
+						YumRepoDir:     "/store/custom/yum.repos.d",
+						YumRepos:       map[string]map[string]string{"cloud-init-daily": {"name": "@cloud-init", "baseurl": "https://k8c.io", "type": "rpm-md"}},
+					},
+				},
+			},
+			expectedCloudConfig: []byte(`#cloud-config
+
+ssh_pwauth: no
+ssh_authorized_keys:
+- 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDR3'
+- 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDR4'
+write_files:
+- path: '/opt/bin/test.service'
+  permissions: '0700'
+  content: |-
+        #!/bin/bash
+        set -xeuo pipefail
+        cloud-init clean
+        cloud-init init
+        systemctl start provision.service
+
+- path: '/opt/bin/setup.service'
+  permissions: '0700'
+  content: |-
+        #!/bin/bash
+        set -xeuo pipefail
+        cloud-init clean
+        cloud-init init
+        systemctl start provision.service
+
+bootcmd:
+- echo hello-world
+- echo hello-osm
+
+runcmd:
+- systemctl restart test.service
+- systemctl restart setup.service
+- systemctl daemon-reload
+
+rh_subscription:
+    password: test_password
+    username: test_username
+
+yum_repos:
+    cloud-init-daily:
+       baseurl: https://k8c.io
+       name: @cloud-init
+       type: rpm-md
+
+yum_repo_dir: /store/custom/yum.repos.d`),
 		},
 	}
 
