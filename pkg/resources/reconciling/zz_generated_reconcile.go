d02a755ea0183dc7499aa647f82c641774c867ba
Waleed Malik <ahmedwaleedmalik@gmail.com>
2023-01-03 13:36:42 +0500

    Add license verification script
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/resources/reconciling/zz_generated_reconcile.go
+++ pkg/resources/reconciling/zz_generated_reconcile.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 The Operating System Manager contributors.
+Copyright 2023 The Operating System Manager contributors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
