9ccc36e23e0c912fc770e319e030e35c3f7ce8c3
Waleed Malik <ahmedwaleedmalik@gmail.com>
2023-01-19 13:59:42 +0500

    Update enqueue conditions for OSP controller (#254)
    * Update watches for OSP controller
    
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>
    
    * Refactored code
    
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>
    
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

---
+++ pkg/util/predicate/predicate.go
@@ -0,0 +1,51 @@
+/*
+Copyright 2023 The Operating System Manager contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package predicate
+
+import (
+	ctrlruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+// ByNamespace returns a predicate func that only includes objects in the given namespace.
+func ByNamespace(namespace string) predicate.Funcs {
+	return Factory(func(o ctrlruntimeclient.Object) bool {
+		return o.GetNamespace() == namespace
+	})
+}
+
+// Factory returns a predicate func that applies the given filter function
+// on CREATE, UPDATE and DELETE events. For UPDATE events, the filter is applied
+// to both the old and new object and OR's the result.
+func Factory(filter func(o ctrlruntimeclient.Object) bool) predicate.Funcs {
+	if filter == nil {
+		return predicate.Funcs{}
+	}
+
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			return filter(e.Object)
+		},
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return filter(e.ObjectOld) || filter(e.ObjectNew)
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return filter(e.Object)
+		},
+	}
+}
