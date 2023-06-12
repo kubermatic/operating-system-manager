a8298482fededc9267faf6942ff029aa52d6a57d
Waleed Malik <ahmedwaleedmalik@gmail.com>
2023-06-12 15:42:36 +0500

    Update K8s dependencies to 1.27.2
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- pkg/controllers/osc/osc_reconciler_test.go
+++ pkg/controllers/osc/osc_reconciler_test.go
@@ -719,10 +719,6 @@ func TestMachineDeploymentDeletion(t *testing.T) {
 				secret); err != nil {
 				t.Fatalf("failed to get provisioning secret: %v", err)
 			}
-
-			// Add deletionTimestamp to Machinedeployment to queue it up for deletion
-			md.ObjectMeta.DeletionTimestamp = &metav1.Time{Time: time.Now()}
-
 			// Reconcile to trigger delete workflow
 			_, err := reconciler.handleMachineDeploymentCleanup(ctx, md)
 			if err != nil {
