2a95a4e30d454fc9eb52471a241d3634358c2443
Mohamed Rafraf <81432497+mohamed-rafraf@users.noreply.github.com>
2024-05-29 14:52:08 +0100

    add baremetal provider (#393)
    update default osps
    
    update
    
    fix yamllint

--- pkg/cloudprovider/cloudprovider.go
+++ pkg/cloudprovider/cloudprovider.go
@@ -70,7 +70,7 @@ func GetCloudConfig(external bool, pconfig providerconfigtypes.Config, kubeletVe
 	case osmv1alpha1.CloudProviderAlibaba, osmv1alpha1.CloudProviderAnexia, osmv1alpha1.CloudProviderDigitalocean,
 		osmv1alpha1.CloudProviderHetzner, osmv1alpha1.CloudProviderLinode, osmv1alpha1.CloudProviderEquinixMetal,
 		osmv1alpha1.CloudProviderScaleway, osmv1alpha1.CloudProviderNutanix, osmv1alpha1.CloudProviderVMwareCloudDirector,
-		osmv1alpha1.CloudProviderOpenNebula, osmv1alpha1.CloudProviderEdge:
+		osmv1alpha1.CloudProviderOpenNebula, osmv1alpha1.CloudProviderEdge, osmv1alpha1.CloudProviderBaremetal:
 		return "", nil
 	}
 
