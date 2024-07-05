8dca03892e5d945393f7740f9226a1e5b1e8cb86
archups <archupsawant@gmail.com>
2024-07-06 00:06:32 +0530

    Bump Go version to 1.22.5
    Signed-off-by: archups <archupsawant@gmail.com>

--- hack/update-crds-openapi.sh
+++ hack/update-crds-openapi.sh
@@ -19,7 +19,7 @@ set -euo pipefail
 cd $(dirname $0)/..
 source hack/lib.sh
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.22-node-20-9 containerize ./hack/update-crds-openapi.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.22-node-20-11 containerize ./hack/update-crds-openapi.sh
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE}")
 
 echodate "Creating vendor directory"
